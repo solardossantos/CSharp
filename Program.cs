32638bf1d88ba62ddd2c826410faee2a6d554f55
solardossantos <107075770+solardossantos@users.noreply.github.com>
2022-09-26 16:54:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace MeuApp
+{
+
+
+    class Program
+     {
+
+         static void Main(string[] args)
+         {
+            int inteiro = 100;
+            float real = 25.5f;
+
+            real = inteiro; // 100.0f
+            inteiro = (int)real;
+            console.WriteLine(inteiro);
+                              
+        
+     }
+
+     }
+}
