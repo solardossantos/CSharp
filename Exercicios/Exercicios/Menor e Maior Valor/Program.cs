8a8a05f496f26062a6a6be233329b711f11aea05
solardossantos <negodossantos@protonmail.com>
2022-11-14 15:18:42 -0300

    Amém Jah

---
+++ Exercicios/Exercicios/Menor e Maior Valor/Program.cs
@@ -0,0 +1,54 @@
+﻿// Armazenando valores
+
+Console.WriteLine("Informe o primeiro valor:");
+float n1 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o segundo valor:");
+float n2 = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe o terceiro valor:");
+float n3 = float.Parse(Console.ReadLine());
+
+float maiorValor = 0;
+
+// Criando a lógica de maiorValor
+
+if (n1 > n2 && n1 > n3)
+{
+    maiorValor = n1;
+}
+
+else if (n2 > n1 && n2 > n3)
+{
+    maiorValor = n2;
+}
+
+else
+{
+    maiorValor = n3;
+}
+
+// Criando a lógica de menorValor
+float menorValor = 0;
+
+
+if (n1 < n2 && n1 < n3)
+{
+    menorValor = n1;
+}
+
+else if (n2 < n1 && n2 < n3)
+{
+    menorValor = n2;
+}
+
+else
+{
+    menorValor = n3;
+}
+
+// Exibindo o resultado final
+
+Console.WriteLine($"O maior valor entre {n1}, {n2} e {n3} é {maiorValor}");
+Console.WriteLine($"O menor valor entre {n1}, {n2} e {n3} é {menorValor}");
+Console.ReadLine();
---
+++ Exercicios/Exercicios/Par ou Impar/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Informe um número inteiro:");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine($"O número {num} é par");
+}
+else
+{
+    Console.WriteLine($"O número {num} é ímpar");
+}
+Console.ReadLine();
