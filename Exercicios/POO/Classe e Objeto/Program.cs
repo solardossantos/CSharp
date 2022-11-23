6ff32eabe0f0cbd19b5c9d64db2bd3b14bb6b1a1
solardossantos <negodossantos@protonmail.com>
2022-11-23 15:59:54 -0300

    Iniciando POO

---
+++ Exercicios/POO/Classe e Objeto/Program.cs
@@ -0,0 +1,17 @@
+﻿// Tipo de Referência
+// -> Endereço dos dados
+Console.WriteLine("Hello World");
+
+class Customer
+{
+    private string name;
+}
+
+class Pagamento
+{
+    // Propriedades
+    private DateTime Vencimento;
+
+    //Métodos
+    void Pagar(){}
+}
